b880b6a4a4f512b994952b68b97c5c6f9871d5c7
Giovanna Teles <giovannateles312@gmail.com>
2026-03-06 11:57:09 -0300

    Initial commit - ASP.NET 6 Todo API

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Get(
+            [FromServices] AppDbContext context)
+        => Ok(context.Todos.ToList());
+        
+
+        [HttpPost ("/")]
+        public IActionResult Post(
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return Created($"/{todo.Id}",todo);
+        }
+
+        [HttpGet]
+        [Route("/{id:int}")]
+        public IActionResult GetItemId(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var todos = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todos == null)
+                return NotFound();
+            return Ok(todos);
+        }
+
+        [HttpPut("/{id:int}")]
+        public IActionResult Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x =>x.Id == id);
+            if (model == null)
+                return NotFound();
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return Ok(model);
+        
+        }
+
+
+        [HttpDelete("/{id:int}")]
+        public IActionResult Put(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return NotFound();
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return Ok(model);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Todo.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
